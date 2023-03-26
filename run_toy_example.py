11802bc0dcda3d59cc709bc484d4a84aaa86d2aa
damaf <47390834+damaf@users.noreply.github.com>
2023-03-26 21:29:13 +0200

    Update run_toy_example.py

--- run_toy_example.py
+++ run_toy_example.py
@@ -4,7 +4,7 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), \
-                                             "../src")))
+                                             "src")))
 import time
 import numpy as np
 import pickle
